893bcce505154ae2a6b84be8d5a0834164d6c003
Servayejc <servayejc@gmail.com>
2022-08-30 13:54:56 -0400

    Update main.cpp
    support server in AP_STA mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
   Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-now-esp8266-nodemcu-arduino-ide/
+  Complete project details at https://RandomNerdTutorials.com/esp-now-two-way-communication-esp8266-nodemcu/
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
@@ -8,61 +8,66 @@
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 */
-#include <arduino.h>
+
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-// REPLACE WITH RECEIVER MAC Address
-//uint8_t broadcastAddress[] = {0x78, 0xE3, 0x6D, 0x09, 0xEE, 0x84};
-//uint8_t broadcastAddress[] = {0x84, 0xf3, 0xeb, 0xee, 0x42, 0xce};
-//uint8_t broadcastAddress[] = {0x84, 0xf3, 0xeb, 0x80, 0xef, 0xda};
-uint8_t serverAddress[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+uint8_t channel = 1;
+int readingId = 0;
+int id = 2;
 
-int channel = 1;
+unsigned long currentMillis = millis(); 
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 2000;  // send readings timer
+
+uint8_t broadcastAddressX[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 enum PairingStatus {PAIR_REQUEST, PAIR_REQUESTED, PAIR_PAIRED, };
 PairingStatus pairingStatus = PAIR_REQUEST;
 
-unsigned long previousMillis = 0; 
-unsigned long start;
+
+// Define variables to store DHT readings to be sent
+float temperature;
+float humidity;
+
+// Define variables to store incoming readings
+float incomingTemp;
+float incomingHum;
+int incomingReadingsId;
+
+// Updates DHT readings every 10 seconds
+//const long interval = 10000; 
+unsigned long previousMillis = 0;    // will store last time DHT was updated 
+
+//Structure example to send data
+//Must match the receiver structure
 typedef struct struct_message {
-    int id;
-    float temp;
-    float hum;
-    int readingId;
+  uint8_t msgType;
+  uint8_t id;
+  float temp;
+  float hum;
+  unsigned int readingId;
 } struct_message;
 
-// structure size must be different from message size
-typedef struct struct_pairing {
-    int id;
-    int channel;
+typedef struct struct_pairing {       // new structure for pairing
+    uint8_t msgType;
+    uint8_t id;
+    uint8_t macAddr[6];
+    uint8_t channel;
 } struct_pairing;
+
 
 // Create a struct_message called myData
 struct_message myData;
 struct_message incomingReadings;
 struct_pairing pairingData;
 
+
 #define BOARD_ID 2
 #define SERVER_ID 0
+unsigned long start;
 
-unsigned long currentMillis = millis(); 
-unsigned long lastTime = 0;  
-unsigned long timerDelay = 2000;  // send readings timer
-
-void printMAC(const uint8_t * mac_addr){
-  char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.print(macStr);
-}
-
-void addPeer(uint8_t * mac_addr, int chan){
-  esp_now_del_peer(serverAddress);
-  esp_now_add_peer(mac_addr, ESP_NOW_ROLE_COMBO, chan, NULL, 0);
-  memcpy(serverAddress, mac_addr, sizeof(uint8_t[6]));
-}
-
+// Callback when data is sent
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("Last Packet Send Status: ");
   if (sendStatus == 0){
@@ -73,45 +78,67 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   }
 }
 
-void OnDataRecv(uint8_t * mac_addr, uint8_t *incomingData, uint8_t len) {
-  if (len == sizeof(myData)){    
-    memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
-    Serial.print("Bytes received: ");
-    Serial.println(len);
-    Serial.println(incomingReadings.id);
-    Serial.println(incomingReadings.temp);
-    Serial.println(incomingReadings.hum);
-    Serial.println(incomingReadings.readingId);
+void printMAC(const uint8_t * mac_addr){
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+}
+
+void printIncomingReadings(){
+  // Display Readings in Serial Monitor
+  Serial.println("INCOMING READINGS");
+  Serial.print("Temperature: ");
+  Serial.print(incomingTemp);
+  Serial.println(" ºC");
+  Serial.print("Humidity: ");
+  Serial.print(incomingHum);
+  Serial.println(" %");
+  Serial.print("Led: ");
+  Serial.print(incomingReadingsId);
   }
+
+// Callback when data is received
+void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+  if (len == sizeof(myData)){ 
+    memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+    Serial.print(len);
+    Serial.print(" bytes received from: ");
+    printMAC(mac);
+    Serial.println();
+    incomingTemp = incomingReadings.temp;
+    incomingHum = incomingReadings.hum;
+    printIncomingReadings();
+    
+    if (incomingReadings.readingId % 2 == 1){
+      digitalWrite(LED_BUILTIN, LOW);
+    } else { 
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+  }
+
   if (len == sizeof(pairingData)){  // we received pairing request from server
     memcpy(&pairingData, incomingData, sizeof(pairingData));
-    if (pairingData.id == SERVER_ID) {                // the message comes from server
+    if (pairingData.id == 0) {                // the message comes from server
       Serial.print("Pairing done for ");
-      printMAC(mac_addr);
+      printMAC(pairingData.macAddr);
       Serial.print(" on channel " );
       Serial.print(pairingData.channel);    // channel used by the server
       Serial.print(" in ");
       Serial.print(millis()-start);
       Serial.println("ms");
-      addPeer(mac_addr, pairingData.channel); // add the server to the peer list 
-      pairingStatus = PAIR_PAIRED;
-      
-                  // set the pairing status
+      esp_now_del_peer(pairingData.macAddr);
+      esp_now_del_peer(mac);
+      esp_now_add_peer(pairingData.macAddr, ESP_NOW_ROLE_COMBO, pairingData.channel, NULL, 0); // add the server to the peer list 
+      pairingStatus = PAIR_PAIRED ;            // set the pairing status
     }
   }  
 }
 
-void setup() {
-  // Init Serial Monitor
-  Serial.begin(74880);
-  
-  myData.id = 200;
-  myData.hum = 50;
-  myData.temp = 22;
-  myData.readingId = 100;
-
-  pairingStatus = PAIR_REQUEST;
-  start = millis();
+void getReadings(){
+  // Read Temperature
+  temperature = 22.5;
+  humidity = 55.5;
 }
 
 PairingStatus autoPairing(){
@@ -142,22 +169,22 @@ PairingStatus autoPairing(){
     // set pairing data to send to the server
     pairingData.id = BOARD_ID;     
     pairingData.channel = channel;
-
+    previousMillis = millis();
     // add peer and send request
-    esp_now_del_peer(serverAddress);
-    addPeer(serverAddress, channel);
-    esp_now_send(serverAddress, (uint8_t *) &pairingData, sizeof(pairingData));
+    Serial.println(esp_now_send(broadcastAddressX, (uint8_t *) &pairingData, sizeof(pairingData)));
     pairingStatus = PAIR_REQUESTED;
     break;
 
   case PAIR_REQUESTED:
     // time out to allow receiving response from server
     currentMillis = millis();
-    if(currentMillis - previousMillis > 50) {
+    if(currentMillis - previousMillis > 100) {
       previousMillis = currentMillis;
-      // time out expited,  try next channel
+      // time out expired,  try next channel
       channel ++;
-      if (channel > 12) {channel = 0;}
+      if (channel > 11) {
+        channel = 0;
+      }
       pairingStatus = PAIR_REQUEST; 
     }
     break;
@@ -169,18 +196,52 @@ PairingStatus autoPairing(){
   return pairingStatus;
 } 
 
-void loop() {
+
+
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(74880);
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Init DHT sensor
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  Serial.println(WiFi.macAddress());
+  WiFi.disconnect();
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Set ESP-NOW Role
+  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+    
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+  esp_now_register_send_cb(OnDataSent);
+
+  pairingData.id = 2;
+}
+ 
+void loop() { 
   if (autoPairing() == PAIR_PAIRED) { 
-    if ((millis() - lastTime) > timerDelay) {
-      // Send message via ESP-NOW
-      Serial.print("Sending message to ");
-      printMAC(serverAddress);
-      Serial.print(" on port ");
-      Serial.println(channel);
-     // addPeer(serverAddress, channel);
-      esp_now_send(serverAddress, (uint8_t *) &myData, sizeof(myData));
-      myData.readingId++;
-      lastTime = millis();
-    }  
+    static unsigned long lastEventTime = millis();
+    static const unsigned long EVENT_INTERVAL_MS = 10000;
+    if ((millis() - lastEventTime) > EVENT_INTERVAL_MS) {
+      Serial.print(".");
+      getReadings();
+
+      //Set values to send
+      myData.id = 1;
+      myData.temp = temperature;
+      myData.hum = humidity;
+      myData.readingId = readingId ++;
+      
+      // Send message via ESP-NOW to all peers 
+      esp_now_send(pairingData.macAddr, (uint8_t *) &myData, sizeof(myData));
+      lastEventTime = millis();
+    }
   }
 }
